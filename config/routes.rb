89f760fe197453c2ec006c55e478c395201d3379
AnnaMaria Jansen <a.jansen@burningbalance.com>
2020-11-17 15:27:20 +0100

    to get a clean status

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :partners, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
-    resources :booking, only: [:index, :show, :new, :create]
+    resources :bookings, only: [:index, :show, :new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
