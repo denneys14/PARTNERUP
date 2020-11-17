5afc635e86c5f11207b0826df2256b68b93e1067
Johanna Fritzmeier <johanna.fritzmeier@me.com>
2020-11-17 12:11:41 +0100

    added  destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :partners, only: [:index, :show, :new, :create] do
+  resources :partners, only: [:index, :show, :new, :create, :destroy] do
     resources :booking, only: [:index, :show, :new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
