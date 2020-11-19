3d43718865a585a784938dd624b353fb74256385
Arthur Birnstiel abirnstiel@icloud.com <abirnstiel@icloud.com>
2020-11-19 14:21:30 +0100

    added avatar url to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_120510) do
+ActiveRecord::Schema.define(version: 2020_11_19_130851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_120510) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "type"
+    t.string "avatar_url", default: "https://www.vippng.com/png/detail/416-4161690_empty-profile-picture-blank-avatar-image-circle.png"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
