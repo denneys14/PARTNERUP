b4b1819411a3d472d6717718768d5b34c860414e
AnnaMaria Jansen <a.jansen@burningbalance.com>
2020-11-20 11:31:32 +0100

    daschboard policy

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,7 @@
 class BookingsController < ApplicationController
   def index
     @bookings = Booking.where(user: current_user)
+    @partners = Partner.where(user: current_user)
   end
 
   def show
