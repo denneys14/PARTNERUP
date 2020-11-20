fb2556fedcad3ec71127e4745db28b2dab6b6ae4
Sophia Denney <sophiad14@kabelmail.de>
2020-11-20 12:33:44 +0100

    inputs

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -4,4 +4,8 @@ class Partner < ApplicationRecord
   has_one_attached :photo
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  validates :name, presence: true
+  validates :duty, presence: true
+  validates :description, presence: true
+  validates :address, presence: true
 end
